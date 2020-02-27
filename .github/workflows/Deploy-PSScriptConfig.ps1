84119630aaeeed1646ee415925fe8174f2ad84d3
abodePS <ab@atwork-it.com>
2020-02-27 15:23:34 +0100

    :art: Improving structure

---
+++ .github/workflows/Deploy-PSScriptConfig.ps1
@@ -0,0 +1,11 @@
+  
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+$nuGetApiKey = $env:PSGALLERY_TOKEN
+
+try{
+    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force
+}
+catch {
+    throw $_
+}
