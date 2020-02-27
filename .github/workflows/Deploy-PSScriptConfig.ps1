125ba730f37263fdd5bfe86850878050c4c910ee
abodePS <ab@atwork-it.com>
2020-02-27 18:01:41 +0100

    test

--- .github/workflows/Deploy-PSScriptConfig.ps1
+++ .github/workflows/Deploy-PSScriptConfig.ps1
@@ -1,7 +1,8 @@
-  
-Write-Host 'Im in'
+
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Import-Module /home/runner/work/PSScriptConfig/PSScriptConfig -Force
+$modulePath = (Get-Module PSScriptConfig).path
 
 $nuGetApiKey = $env:PSGALLERY_TOKEN
 
