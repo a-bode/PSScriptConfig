6e6d628d7f48057fc88c08750e3a122dc2ea7872
abodePS <ab@atwork-it.com>
2020-02-28 13:11:15 +0100

    test

--- .github/workflows/Deploy-PSScriptConfig.ps1
+++ .github/workflows/Deploy-PSScriptConfig.ps1
@@ -1,13 +1,20 @@
-
+$modulePath = "/home/runner/work/PSScriptConfig/PSScriptConfig/main/PSScriptConfig"
+$nuGetApiKey = $env:PSGALLERY_TOKEN
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-$modulePath = "/home/runner/work/PSScriptConfig/PSScriptConfig/main/PSScriptConfig"
+#Install current version from PSGallery
+Install-Module -Name PSScriptConfig -AllowPrerelease
+[version]$version = (Get-Module -Name PSScriptConfig).Version
 
-$nuGetApiKey = $env:PSGALLERY_TOKEN
+#Generate new version
+[version]$newVersion = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build +1)
+$manifest = Import-PowerShellDataFile (Join-Path $modulePath -ChildPath "PSScriptConfig.psd1")
+Update-ModuleManifest -Path (Join-Path $modulePath -ChildPath "PSScriptConfig.psd1") -ModuleVersion $newVersion
+
 
 try{
-    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force
+   Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force
 }
 catch {
     throw $_
