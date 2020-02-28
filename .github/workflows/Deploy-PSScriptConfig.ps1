b008d1a7d590707ed738a1b46a037bc578dcc305
abodePS <ab@atwork-it.com>
2020-02-28 15:37:15 +0100

    update deploy

--- .github/workflows/Deploy-PSScriptConfig.ps1
+++ .github/workflows/Deploy-PSScriptConfig.ps1
@@ -3,15 +3,21 @@ $nuGetApiKey = $env:PSGALLERY_TOKEN
 
 
 
-Get-PSRepository -Name psgallery |Set-PSRepository -InstallationPolicy Trusted
+# Get-PSRepository -Name psgallery |Set-PSRepository -InstallationPolicy Trusted
 
-#Install current version from PSGallery
-Install-Module -Name PSScriptConfig -Force
-Import-Module PSScriptConfig -Force
-[version]$version = (Get-Module -Name PSScriptConfig).Version
+# #Install current version from PSGallery
+# Install-Module -Name PSScriptConfig -Force
+# Import-Module PSScriptConfig -Force
+# [version]$version = (Get-Module -Name PSScriptConfig).Version
+
 
 #Generate new version
-[version]$newVersion = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build +1)
+# [version]$newVersion = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build +1)
+
+
+[version]$newVersion = (git tag | tail -1) -replace 'v'
+
+
 Update-ModuleManifest -Path (Join-Path $modulePath -ChildPath "PSScriptConfig.psd1") -ModuleVersion $newVersion
 
 
